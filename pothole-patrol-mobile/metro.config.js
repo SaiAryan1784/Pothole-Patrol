12c58d95a20ce3cbd6c6c5f245dd87b96a368020
SaiAryan1784 <saiaryan.goswami1784@gmail.com>
2026-04-09 17:59:35 +0530

    feat: add TFLite model asset, configure metro bundler for model files, and update environment variables for production deployment

--- pothole-patrol-mobile/metro.config.js
+++ pothole-patrol-mobile/metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure .tflite model files are bundled as assets
+config.resolver.assetExts.push('tflite');
+
 module.exports = withNativeWind(config, { input: "./app/global.css" });
